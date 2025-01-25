3c182e80b8a33c972011db3df9c6693ec533df78
Téssio Carvalho <tcarvalho.gest@hotmail.com>
2025-01-25 18:20:59 -0300

    :sparkles: feat: Entities created

---
+++ src/main/java/com/tessio/tournament/entities/AdultCategory.java
@@ -0,0 +1,78 @@
+package com.tessio.tournament.entities;
+
+public class AdultCategory {
+	
+	private String rooster;
+	private String lightFeather;
+	private String feather;
+	private String light;
+	private String middle;
+	private String mediumHeavy;
+	private String heavy;
+	private String superHeavy;
+	private String ultraHeavy;
+	private String openClass;
+	public String getRooster() {
+		return rooster;
+	}
+	public void setRooster(String rooster) {
+		this.rooster = rooster;
+	}
+	public String getLightFeather() {
+		return lightFeather;
+	}
+	public void setLightFeather(String lightFeather) {
+		this.lightFeather = lightFeather;
+	}
+	public String getFeather() {
+		return feather;
+	}
+	public void setFeather(String feather) {
+		this.feather = feather;
+	}
+	public String getLight() {
+		return light;
+	}
+	public void setLight(String light) {
+		this.light = light;
+	}
+	public String getMiddle() {
+		return middle;
+	}
+	public void setMiddle(String middle) {
+		this.middle = middle;
+	}
+	public String getMediumHeavy() {
+		return mediumHeavy;
+	}
+	public void setMediumHeavy(String mediumHeavy) {
+		this.mediumHeavy = mediumHeavy;
+	}
+	public String getHeavy() {
+		return heavy;
+	}
+	public void setHeavy(String heavy) {
+		this.heavy = heavy;
+	}
+	public String getSuperHeavy() {
+		return superHeavy;
+	}
+	public void setSuperHeavy(String superHeavy) {
+		this.superHeavy = superHeavy;
+	}
+	public String getUltraHeavy() {
+		return ultraHeavy;
+	}
+	public void setUltraHeavy(String ultraHeavy) {
+		this.ultraHeavy = ultraHeavy;
+	}
+	public String getOpenClass() {
+		return openClass;
+	}
+	public void setOpenClass(String openClass) {
+		this.openClass = openClass;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/tessio/tournament/entities/Athlete.java
@@ -0,0 +1,122 @@
+package com.tessio.tournament.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_atlhete")
+public class Athlete {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String cpf;
+	private String email;
+	private String team;
+	private Float weight;
+	private Integer phone;
+	private Integer age;
+	
+	public Athlete() {}
+
+	public Athlete(Integer id, String name, String cpf, String email, String team, Float weight, Integer phone, Integer age) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.email = email;
+		this.team = team;
+		this.weight = weight;
+		this.phone = phone;
+		this.age = age;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	public Float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Float weight) {
+		this.weight = weight;
+	}
+
+	public Integer getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Integer phone) {
+		this.phone = phone;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Athlete other = (Athlete) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
