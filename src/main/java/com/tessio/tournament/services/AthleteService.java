65d9b1ba0ca4813f595b77c11bf6148421f60acd
Téssio Carvalho <tcarvalho.gest@hotmail.com>
2025-01-26 23:34:07 -0300

    :recycle: refactor: service using DTO class

--- src/main/java/com/tessio/tournament/services/AthleteService.java
+++ src/main/java/com/tessio/tournament/services/AthleteService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tessio.tournament.dto.AthleteDTO;
 import com.tessio.tournament.entities.Athlete;
 import com.tessio.tournament.repositories.AthleteRepository;
 
@@ -14,9 +15,9 @@ public class AthleteService {
 	@Autowired
 	private AthleteRepository athleteRepository;
 	
-	public List<Athlete> findAll(){
+	public List<AthleteDTO> findAll(){
 		List<Athlete> result = athleteRepository.findAll();
-		return result;
+		return result.stream().map(AthleteDTO::new).toList();
 	}
 	
 }
