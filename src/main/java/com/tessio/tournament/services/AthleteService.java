a15022d7b83d0d74e5dc1cf138aabeb3cc070d6a
Téssio Carvalho <tcarvalho.gest@hotmail.com>
2025-01-28 23:10:27 -0300

    :recycle: refactor: New business rules added

--- src/main/java/com/tessio/tournament/services/AthleteService.java
+++ src/main/java/com/tessio/tournament/services/AthleteService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.tessio.tournament.dto.AthleteAgeDTO;
 import com.tessio.tournament.dto.AthleteDTO;
+import com.tessio.tournament.dto.HeavyWeightDTO;
 import com.tessio.tournament.entities.Athlete;
 import com.tessio.tournament.repositories.AthleteRepository;
 
@@ -15,9 +18,22 @@ public class AthleteService {
 	@Autowired
 	private AthleteRepository athleteRepository;
 	
+	@Transactional(readOnly = true)
 	public List<AthleteDTO> findAll(){
 		List<Athlete> result = athleteRepository.findAll();
-		return result.stream().map(AthleteDTO::new).toList();
+		return result.stream().map(x -> new AthleteDTO(x)).toList();
 	}
 	
+	@Transactional(readOnly = true)
+	public List<HeavyWeightDTO> findHeavyWeight(){
+		List<Athlete> result = athleteRepository.findByWeightBetween(83.90, 94.90);
+		return result.stream().map(x -> new HeavyWeightDTO(x)).toList();
+	}	
+	
+	@Transactional(readOnly = true)
+	public List<AthleteAgeDTO> findByAgeBetween(Integer ageMin, Integer ageMax) {
+        List<Athlete> result = athleteRepository.findByAgeBetween(ageMin, ageMax);
+        return result.stream().map(x -> new AthleteAgeDTO(x)).toList();
+    }	
+		
 }
