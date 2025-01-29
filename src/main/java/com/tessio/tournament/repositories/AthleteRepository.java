dcc60d262ae5286d74a928bd951bbea6875aac4f
Téssio Carvalho <tcarvalho.gest@hotmail.com>
2025-01-28 22:36:05 -0300

    :sparkles: feat: find by added

--- src/main/java/com/tessio/tournament/repositories/AthleteRepository.java
+++ src/main/java/com/tessio/tournament/repositories/AthleteRepository.java
@@ -1,9 +1,17 @@
 package com.tessio.tournament.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tessio.tournament.entities.Athlete;
 
 public interface AthleteRepository extends JpaRepository<Athlete, Integer>{
+	
+	List<Athlete> findByWeightBetween(Double weightMin, Double weightMax);
+	
+	List<Athlete> findByAgeBetween(Integer ageMin, Integer ageMax);
+	
+	List<Athlete> findByTeam(String team);
 
 }
